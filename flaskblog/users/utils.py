a62bffd1f533846c3fa1850cdeba35ed8606c670
Sridhar Iyer <siyer@cccis.com>
2019-11-06 14:54:24 -0600

    Removed threading from send mail as it throws err due to current_app. Will fix later

--- flaskblog/users/utils.py
+++ flaskblog/users/utils.py
@@ -20,22 +20,44 @@ def save_picture(form_picture):
     i.save(pic_path)
     return pic_filename
 
+# -------------------------------------------------------------------------------------
+# Following 2 functions, related to threading are not working due to an error message:
+# RuntimeError: Working outside of application context.
 
-def send_async_email(app, msg):
-    with app.app_context():
-        mail.send(msg)
+# This typically means that you attempted to use functionality that needed
+# to interface with the current application object in some way. To solve
+# this, set up an application context with app.app_context().  See the
+# documentation for more information.
+
+# TODO: Fix later
+# -------------------------------------------------------------------------------------
+# def send_async_email(app, msg):
+#     with app.app_context():
+#         mail.send(msg)
+#
+#
+# def send_reset_email(user):
+#     token = user.get_reset_token()
+#     msg = Message('Password Reset Request',
+#                   sender='noreply@flaskblogdemo.com', recipients=[user.email])
+#     msg.body = f'''
+# To reset your password, visit the following link:
+# {url_for('users.reset_token', token=token, _external=True)}
+#
+# If you did not receive this email then simply ignore this email and no changes will be made.
+#     '''
+#
+#     # mail.send(msg)
+#     Thread(target=send_async_email, args=(current_app, msg)).start()
 
 
 def send_reset_email(user):
     token = user.get_reset_token()
     msg = Message('Password Reset Request',
-                  sender='noreply@flaskblogdemo.com', recipients=[user.email])
-    msg.body = f'''
-To reset your password, visit the following link:
+                  sender='noreply@demo.com',
+                  recipients=[user.email])
+    msg.body = f'''To reset your password, visit the following link:
 {url_for('users.reset_token', token=token, _external=True)}
-
-If you did not receive this email then simply ignore this email and no changes will be made.
-    '''
-
-    # mail.send(msg)
-    Thread(target=send_async_email, args=(current_app, msg)).start()
+If you did not make this request then simply ignore this email and no changes will be made.
+'''
+    mail.send(msg)
