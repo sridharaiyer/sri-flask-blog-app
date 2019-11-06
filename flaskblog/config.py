b5e29ae43d31eaf96dc172d29866a1debc9e0443
Sridhar Iyer <siyer@cccis.com>
2019-11-06 14:34:34 -0600

    moved config info to /etc/sri-flask-blog-app-config.json

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,13 +1,17 @@
 import os
+import json
+
+with open('/etc/sri-flask-blog-app-config.json') as config_file:
+    config = json.load(config_file)
 
 
 class Config:
     """docstring for Config."""
 
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = config.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = config.get('SQLALCHEMY_DATABASE_URI')
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('SRI_GOOGLEAPI_USER')
-    MAIL_PASSWORD = os.environ.get('SRI_GOOGLEAPI_PWD')
+    MAIL_USERNAME = config.get('SRI_GOOGLEAPI_USER')
+    MAIL_PASSWORD = config.get('SRI_GOOGLEAPI_PWD')
